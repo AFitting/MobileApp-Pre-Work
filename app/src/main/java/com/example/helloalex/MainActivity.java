fc4ce1a89a7870b3404112ae61fa7c3e11938506
AFitting <asf45@humboldt.edu>
2020-01-23 19:05:15 -0800

    Pre-Work for CS 480

---
+++ app/src/main/java/com/example/helloalex/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.helloalex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.i("Alex", "Button Clicked");
+                ((TextView) findViewById(R.id.textView)).setTextColor(
+                        getResources().getColor(R.color.colorPrimary));
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.activity_main).setBackgroundColor(
+                        getResources().getColor(R.color.orange));
+            }
+        });
+    }
+
+}
